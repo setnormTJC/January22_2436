462344412cc49281f596e1a56f33946a65fcfc05
Work <Work@MSI>
2025-01-27 12:31:04 -0600

    Mid class on Jan 27

--- January22_2436/January22_2436.cpp
+++ January22_2436/January22_2436.cpp
@@ -2,7 +2,7 @@
 // January22_2436.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 #include"searchAlgos.h"
-
+#include<chrono>
 
 void demoSearchingThrough2DStringArray()
 {
@@ -19,15 +19,50 @@ void demoSearchingThrough2DStringArray()
 }
 
 
+void demoMeasuringExecutionTime()
+{
+	auto start = std::chrono::high_resolution_clock::now(); 
+	//chrono is Latin/Greek and means "related to time" 
+	//how long (in NANOSECONDS) does it take for my program to count to one billion? 
+	for (int i = 0; i < 1'000'000'000; ++i)
+	{
+		//nothin'
+	}
+	auto end = std::chrono::high_resolution_clock::now();
+
+	cout.imbue(std::locale(""));
+	//puts a comma every three digits for nums >> 1 (on my machine)
+
+	cout << "It took this much time: " << (end - start).count() << "\n";
+}
+
 
 int main()
 {
+
+	demoMeasuringExecutionTime(); 
+
+	std::cin.get();//similar to system("pause")
+
+
 	//cout << RAND_MAX << "\n"; //a limitiation of rand() (a C function)
 
 	//cout << "Time(nullptr) yields: " << time(nullptr) << "\n";
 
-	auto oneThousandRandomNums = generateRandomNumbersBetwixt0AndN(1000);
+
+	constexpr int N1 = 1'000; 
+	//const? 
+	constexpr int N2 = 10'000;
+	auto oneThousandRandomNums = generateRandomNumbersBetwixt0AndN(N1);
+	//auto tenThousandRandomNums = generateRandomNumbersBetwixt0AndN(N2); 
+
+
+
+	//search(oneThousandRandomNums, someNumberBetween0AndOneThousand);
+
 
 	printOneDVector(oneThousandRandomNums); 
+
+
 }
 
